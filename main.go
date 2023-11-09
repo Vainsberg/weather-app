9de51bd3b082405c9eed9622bdec9bb1b6b9d35c
Stas Vasilyev <vasilyev@iMac-Stas.local>
2023-11-09 23:43:50 +0300

    Create API weather

--- main.go
+++ main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+
+	query := r.URL.Query()
+	latitudeText := query.Get("latitude")
+	longitudeText := query.Get("longitude")
+
+	if latitudeText == "" || longitudeText == "" {
+		http.Error(w, "Latitude and longitude are required parameters", http.StatusBadRequest)
+		return
+	}
+	weatherAPIURL := "https://api.open-meteo.com/v1/forecast?latitude=" + latitudeText + "&longitude=" + longitudeText
+
+	weatherResp, err := http.Get(weatherAPIURL)
+	if err != nil {
+		log.Println("Error fetching weather data:", err)
+		http.Error(w, "Error fetching weather data", http.StatusInternalServerError)
+		return
+	}
+	defer weatherResp.Body.Close()
+
+	data, err := io.ReadAll(weatherResp.Body)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(data)
+}
 
 func main() {
-	fmt.Println("H")
+	http.HandleFunc("/get_weather", handler)
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Ошибка запуска сервера:", err)
+	}
 }
